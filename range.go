bc5dab953b272ffdce4b769428c2cc891b759d15
Mike Jensen <jent@threadly.org>
2025-03-18 17:44:54 -0600

    fix: Further tick reduction for dense datasets
    This contines the change done in v0.5.3, further reducing the tick count. With the last change the difference in tick to label count can make it hard to read.
    
    This change enforces a ratio of tick to label counts with the goal of making the label to tick position clearer to read.
    
    Unfortunately the fallout from v0.5.2 has required several iterations to get the readability right.

--- range.go
+++ range.go
@@ -244,7 +244,7 @@ func calculateCategoryAxisRange(p *Painter, axisSize int, isVertical bool, extra
 		}
 	}
 	// ensure each tick has at least 8px of space
-	maxTickCount := axisSize / 8
+	maxTickCount := chartdraw.MinInt(axisSize/8, labelCount*4)
 	for dataCount > maxTickCount {
 		// cut in half until we within our limit, ensuring the first and last tick always remain
 		dataCount = minimumAxisLabels + ((dataCount - minimumAxisLabels) / 2)
